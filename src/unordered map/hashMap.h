c308b264b9711421fdc717e11002f662c1e41027
Yassen <46229625+YassenA@users.noreply.github.com>
2023-02-17 19:53:38 +0000

    Update hashMap.h

--- src/unordered map/hashMap.h
+++ src/unordered map/hashMap.h
@@ -8,39 +8,52 @@ namespace container {
 	class HashTable {
 		private:
 			using hashTable = std::vector<std::list<std::pair<const Key, Type>>>;
-
+		
 			std::size_t m_size{};		
 			Hash m_hash								  
 
 			std::vector<std::list<std::pair<const Key, Type>>> m_table; 
 		public:
+			using valueType = std::pair<const Key, mapped_type>;
+		
+			// Default constructor
+			constexpr HashTable(const hasher& hash = Hash())
+				: m_size{ 0 }
+				, m_hash{ hash }
+				, m_bucket_count{ 0 }
 
+			{}
+		
+			// Initialisation constructor
 			constexpr HashTable(const HashTable& other)
 				, m_hash{ other.m_hash }
 				, m_size{ other.m_size }
 				, m_table{ other.m_table }
-			{
-			}
+			{}
 
+			// Swap constructor, itialise object
 			constexpr HashTable(HashTable&& other) noexcept
 				: HashTable() {
 				other.swap(*this);
 			}
-
+			
+			// Swap constructor, overloading assingment operator 
 			constexpr HashTable& operator=(HashTable&& other) noexcept {
 				HashTable temp;
 				other.swap(*this);
 				temp.swap(other);
 				return *this;
 			}
-
+			
+			// Swap keys and values between two maps constructor	
 			constexpr HashTable& operator=(const HashTable& other) {
 				HashTable temp(other);
 				temp.swap(*this);
 				return *this;
 			}
-
-			constexpr HashTable& operator=(std::initializer_list<value_type> list) {
+		
+			// Initialise with key and value pair constructor
+			constexpr HashTable& operator=(std::initializer_list<valueType> list) {
 				HashTable temp{ list };
 				temp.swap(*this);
 				return *this;
