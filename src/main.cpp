1ec42df2dbdc3d9bfd5e553e21a8ffb7fc87728b
YassenA <smojang909@gmail.com>
2023-02-27 21:29:45 +0000

    Cleaned up main and fixed logical error with insertion after empty map

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,63 @@
 #include "unordered-map/hashMap.h"
 
 int main() {
-
-    //UnorderedMap::HashTable<std::string, int> testHash = { {"test1", 54}, {"test2", 5}, {"test3", 657}, {"test4", 523} };
-    //UnorderedMap::HashTable<std::string, int> testHash2 = { {"test1", 54}, {"test2", 5}, {"test3", 657}, {"test4", 523} }; //{ {"test3", 657}, {"test4", 523}, {"test5", 456}, {"test6", 345} };
-    //UnorderedMap::HashTable<std::string, int> testHash3 = { {"test3", 657} };
     UnorderedMap::HashTable<std::string, int> testHash = { {"1-1", 54}, {"1-2", 5}, {"1-3", 65}, {"1-4", 523}, {"1-1", 54} };
     UnorderedMap::HashTable<std::string, int> testHash2 = { {"2-1", 76}, {"2-2", 1} };
-    UnorderedMap::HashTable<std::string, int> testHash3 = { {"3-1", 85834} };
+    UnorderedMap::HashTable<std::string, int> testHash3 = { {"3-1", 85834}, {"3-2", 12} };
+    UnorderedMap::HashTable<std::string, int> testHash4 = { {"4-1", 1238} };
+
+    /// Functionality 
+    // Lookup functions
+    std::cout << testHash.at("1-1") << "\n"; // 54
+    std::cout << testHash.containsKey("1-1") << "\n"; // true (1)
+    std::cout << testHash.containsValue(51223) << "\n"; // false (0)
+    std::cout << testHash.count({"1-1", 54}) << "\n"; // 2
+    std::cout << testHash["1-1"] << "\n"; // 54
+    testHash.printContents();
+    std::cout << testHash.size() << "\n";
+
+    // Modifiers
+    testHash4.clear();
+    testHash4.empty(); // true (1)
+    testHash4.insert({{ "4-2", 5 }, { "4-2", 5 }});
+
+    //testHash4.insert({ { "4-3", 1 }, { "4-4", 183 } });
+    //testHash.printContents();
+    
+    //testHash4.insert({{ "4-3", 1 }, { "4-4", 183 }});
+    // testHash4.insert(testHash);
+    
+    //testHash.emplace();
+
+    testHash3.merge(testHash, testHash2);
+    testHash3 = testHash + testHash2;
+    testHash3.reHash();
+    testHash3.removeByKey("3-1");
+    testHash3.removeByValue(12);
+    testHash3.reserve(30);
+    testHash3.swap(testHash4);
+
+    // Bucket interface
+    std::cout << testHash.bucketSize(2) << "\n"; // 2
+    std::cout << testHash.maxBucketCount() << "\n"; // 768614336404564650
 
 
+   
+    // Other
+    std::cout << testHash.calculateLoadBalance() << "\n"; // 0.25
+    std::cout << testHash.maxLoadFactor() << "\n";
+
+    //testHash.printContents();
+    //std::cout << "\n";
+    //testHash.insert({ "voodo", 234 });
+    //testHash.printContents();
+    //std::cout << "\n\n";
+    //testHash.clear();
+    //std::cout << testHash.empty() << std::endl;
+    //std::cout << "\n\n";
+    //testHash.printContents();
+    
+    //std::cout << << "\n";
 
     //std::cout << testHash["test1"] << std::endl;
     //testHash["test"];
@@ -31,12 +79,12 @@ int main() {
     //testHash.insert(testHash2);
     //testHash.printContents();
 
-    testHash.printContents();
-    testHash.merge(testHash2, testHash3);
-    std::cout << "\n\n";
-    testHash.printContents();
-    std::cout << "\n\n";
-    std::cout << testHash.count({ "1-1", 54 }) << std::endl;
+    //testHash.printContents();
+    //testHash.merge(testHash2, testHash3);
+    //std::cout << "\n\n";
+    //testHash.printContents();
+    //std::cout << "\n\n";
+    //std::cout << testHash.count({ "1-1", 54 }) << std::endl;
 
     //testHash.eraseIf({ "test1", 54 });
     //UnorderedMap::HashTable<std::string, int> testHash4 = testHash + testHash3;
--- src/unordered-map/hashMap.h
+++ src/unordered-map/hashMap.h
@@ -3,13 +3,9 @@
 #include <list>
 #include <vector>
 #include <initializer_list>
-
-#include <ranges>
 #include <iomanip>
 #include <iostream>
-#include <iomanip>
-#include <string>
-#include <array>
+
 namespace UnorderedMap {
 	template<typename Key, typename Type, typename Hash = std::hash<Key>>
 	
@@ -32,16 +28,16 @@ namespace UnorderedMap {
 		template <typename K, typename V>
 		friend std::ostream& operator<<(std::ostream& os, const HashTable<K, V>& hashTableOS);
 
+		// Constructors
+
 		// List initialisation constructor
 		constexpr HashTable(std::initializer_list<valueType> hashList, const Hash& hash = Hash())
 			: mSize{ hashList.size() },
-			mHash{ hash },
-			mBucketCount{ static_cast<uInt>(hashList.size() * growthFactor) },
-			mTable(static_cast<uInt>(hashList.size()* growthFactor))
+			  mHash{ hash },
+			  mBucketCount{ static_cast<uInt>(hashList.size() * growthFactor) },
+			  mTable(static_cast<uInt>(hashList.size()* growthFactor))
 		{
-			for (auto elementPair : hashList) {
-				insert(elementPair);
-			}
+			for (auto elementPair : hashList) { insert(elementPair); }
 		}
 
 		// Default constructor, empty initilisation 
@@ -74,7 +70,7 @@ namespace UnorderedMap {
 			return *this;
 		}
 
-		// Swap keys and values between two maps constructor	
+		// Swap keys and values between two hashes constructor	
 		constexpr HashTable& operator=(const HashTable& otherTable) {
 			HashTable temp(otherTable);
 			temp.swap(*this);
@@ -90,20 +86,24 @@ namespace UnorderedMap {
 
 		// Modifiers
 		constexpr void insert(std::initializer_list<valueType> hashList) {
-			for (auto elementPair : hashList) {
+			for (auto& elementPair : hashList) {
 				insert(elementPair);
 			}
 		}
 		constexpr bool insert(valueType&& value) {
+			if (mBucketCount <= 0) { mBucketCount = 1; reHash();} // If the map is empty
+
 			uInt index{ mHash(value.first) % mBucketCount };
 			auto& currentBucket{ mTable.at(index) };
-			currentBucket.push_front(value);
+			currentBucket.push_front(std::move(value));
 			mSize++;
 
 			if (calculateLoadBalance() > mMaxLoadFactor) { reHash(); }
 			return true;
 		}
 		constexpr bool insert(const valueType& value) {
+			if (mBucketCount <= 0) { mBucketCount = 1; reHash(); } // If the map is empty
+
 			uInt index{ mHash(value.first) % mBucketCount };
 			auto& currentBucket{ mTable.at(index) };
 			currentBucket.push_front(value);
@@ -126,17 +126,13 @@ namespace UnorderedMap {
 			}
 			return this->mTable;
 		}
-		//constexpr Hash insert(Hash value) {
-
-		//}
-
 
 		constexpr void clear() noexcept {
 			mTable.clear();
 			mSize = 0;
 			mBucketCount = 0;
 		}
-		constexpr bool emplace() {}
+		//constexpr bool emplace() {}
 		constexpr bool removeByKey(const Key& key) {
 			uInt index{ mHash(key) % mBucketCount };
 			auto& currentList{ mTable.at(index) };
@@ -176,7 +172,6 @@ namespace UnorderedMap {
 				}
 			}
 		}
-
 		template <typename K, typename V>
 		HashTable& operator+(HashTable<K, V> rightObject) {
 			this->mSize = this->size() + rightObject.size();
@@ -185,10 +180,7 @@ namespace UnorderedMap {
 			return *this;
 
 		}
-		constexpr void merge(HashTable leftObject, HashTable rightObject){
-			*this = (leftObject + rightObject) + *this;
-		}
-
+		constexpr void merge(HashTable leftObject, HashTable rightObject){ *this = (leftObject + rightObject) + *this; }
 
 		// Lookup functions
 		constexpr const Type& at(const Key& key) const {
@@ -236,10 +228,7 @@ namespace UnorderedMap {
 			std::cout << "==Bucket==\nBucket Count: " << this->mBucketCount << "\nMax Bucket Count: " << this->maxBucketCount()
 			          << "\n==Load Factor==\nLoad Blance: " << std::setprecision(8) << this->calculateLoadBalance() << "\nMax Load Factor: " << this->maxLoadFactor() << "\n";
 		}
-		//template <typename T>
-		//constexpr bool find(const T& input) {
-		//	
-		//}
+
 		constexpr uInt count(valueType&& value) const {
 			uInt counter = 0;
 			if (this->containsKey(value.first)) {
@@ -266,12 +255,11 @@ namespace UnorderedMap {
 			return currentList.front().second;
 		}
 		
-		// Comparison functions - the operator!= is implicitly an inverse of the operator== as of C++20
+		// Comparison functions - the != operator is implicitly an inverse of the operator== as of C++20
 		constexpr bool operator==(const HashTable& otherTable) {
 			if (this->size() != otherTable.size() || this->mTable != otherTable.mTable) { return false; }
 			return true;
 		}
-
 		constexpr void reHash() {
 			hashTable tempHash{ mTable };
 			mTable.clear();
@@ -296,17 +284,17 @@ namespace UnorderedMap {
 				}
 			}
 		}
-		constexpr double calculateLoadBalance() const noexcept { return static_cast<double>(mSize / mBucketCount); }
+		constexpr double calculateLoadBalance() const noexcept { return static_cast<double>(mSize) / mBucketCount; }
 		constexpr double maxLoadFactor() const noexcept        { return mMaxLoadFactor; }
 		constexpr double maxLoadFactor(double newLoadFactorVal){ return mMaxLoadFactor = newLoadFactorVal; }
-		constexpr void reserve(uInt reservationAmount) {
+		constexpr void   reserve(uInt reservationAmount) {
 			mBucketCount = reservationAmount;
 			reHash(reservationAmount);
 		}
 		constexpr void swap(HashTable& otherTable) {
-			std::swap(mSize, otherTable.mSize);
-			std::swap(mTable, otherTable.mTable);
-			std::swap(mHash, otherTable.mHash);
+			std::swap(mSize,        otherTable.mSize);
+			std::swap(mTable,       otherTable.mTable);
+			std::swap(mHash,        otherTable.mHash);
 			std::swap(mBucketCount, otherTable.mBucketCount);
 		}
 
